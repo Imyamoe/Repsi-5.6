3b91888eccbf1f97cbc7746a065210b44928311d
Imyamoe <engurd@gmail.com>
2025-07-19 21:13:54 +0200

    Add files via upload

--- Source/RepsiCore/RepsiCore.Build.cs
+++ Source/RepsiCore/RepsiCore.Build.cs
@@ -5,7 +5,7 @@ public class RepsiCore : ModuleRules
 	public RepsiCore(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		bEnforceIWYU = true;
+		IWYUSupport = IWYUSupport.Full;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine" });
 		PrivateDependencyModuleNames.AddRange(new string[] { });
